60887230de3cb1e32c3ab1dc8d77d92f98ba8769
Aarron Walter <awalter@resolvetosavelives.org>
2025-02-06 11:35:42 -0500

    Home page cosmetic tweaks.

--- app/page.js
+++ app/page.js
@@ -171,8 +171,9 @@ export default function Home() {
   return (
 	<div className="container mx-auto p-4">
 	  <h1 className="text-3xl font-bold mb-4 text-gray-600">Talent Directory</h1>
+	  <p className="text-sm text-gray-600 mb-8 pb-8 border-b">Helping great people find great jobs.</p>
 
-	  <div className="mb-4 space-y-4">
+	  <div className="mb-4 space-y-4 mb-8 pb-8 border-b">
 		<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
 		  <input
 			type="text"
